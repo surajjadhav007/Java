289d7e9ba2d1d5ae156bb846bd7490383117fa3e
Suraj Jadhav <sj2961844@gmail.com>
2026-02-13 10:57:49 +0530

    Update java p1.java

--- java p1.java
+++ java p1.java
@@ -5,4 +5,23 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+import java.util.Scanner;
+
+public class MultiplyTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        int result = num1 * num2;
+
+        System.out.println("Multiplication = " + result);
+
+        sc.close();
+    }
+}
 
